b5ee24d0a370a18ff7724a38c4e0e8e80eae4b3e
hashim-777x <syedhashim721@gmail.com>
2026-06-13 23:41:55 +0530

    unsafe

--- memview/main.go
+++ memview/main.go
@@ -19,5 +19,7 @@ type GoodLayout struct {
 func main() {
 	Bad:= BadLayout{}
 	fmt.Println( "bad lay out size : ", unsafe.Sizeof(Bad) )
-	
+	good := GoodLayout{}
+	fmt.Println( "good lay out size : ", unsafe.Sizeof(good) )
 }
+
