1e3d836eb5e836f121c3ca458910bb5c37130e15
hashim-777x <syedhashim721@gmail.com>
2026-06-16 23:56:04 +0530

    tcp port and event listner

--- tinytcp/main.go
+++ tinytcp/main.go
@@ -7,4 +7,13 @@ import (
  
 func main() {
 	fmt.Println("Hello, World!")
+    const PORT = "8080"
+	
+	listener, err := net.Listen("tcp", "localhost:"+PORT)
+	if err != nil {
+		fmt.Println("Error starting TCP server:", err)
+		return
+	}
+	defer listener.Close()
+
 }
